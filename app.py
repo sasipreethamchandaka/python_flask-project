b41b02780f10cad846757a9653b3d7c91491dd88
sasi preetham chandaka <87517348+sasipreethamchandaka@users.noreply.github.com>
2026-01-30 23:34:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "🚀 Flask app deployed and  successfully updated and deployed via GitHub Actions! Enjoy the new version."
+    return "🚀 Flask app  successfully updated and deployed via GitHub Actions! "
 
 
 
